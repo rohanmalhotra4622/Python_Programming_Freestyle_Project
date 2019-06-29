b05043059c74f723d3400a2ce389c3e1a33f6e7f
rohanmalhotra4622 <44009676+rohanmalhotra4622@users.noreply.github.com>
2019-06-29 14:51:43 -0400

    Comparing volatility of securities within portfolio

--- stocks.py
+++ stocks.py
@@ -37,7 +37,7 @@ portfolio = portfolio[:-1]
 #stock2 = input("Please enter stock: ")
 
 end_date = datetime.today()
-beg_date = end_date - timedelta(365)
+beg_date = end_date - timedelta(20)
 
 # Define the Index, this should be the benchmark we want to compare against.  
 print('---------------------------------------------------------')
@@ -54,7 +54,7 @@ sp500.columns = ['Date','sp500']
 #print(sp500)
 
 print('---------------------------------------------------------')
-
+print('MY PORTFOLIO')
 # Convert portfolio, which is a list into a dataframe
 
 df = pd.DataFrame({'Ticker':portfolio} )
@@ -62,7 +62,6 @@ my_portfolio = pdr.get_data_yahoo(portfolio, start=beg_date, end=end_date)
 my_portfolio = my_portfolio['Adj Close'].reset_index()
 
 print(my_portfolio)
-print('MY PORTFOLIO')
 print('---------------------------------------------------------')
 
 #jpm = pdr.get_data_yahoo(symbols=stock1, start=datetime(2019, 4, 10), end=datetime(2019, 6, 24))
@@ -79,7 +78,7 @@ print('---------------------------------------------------------')
 
 
 # Merge the  dataframes
-
+print('MY PORTFOLIO vs. SP500')
 combined_df = sp500.merge(my_portfolio, left_on='Date',right_on= 'Date', how='inner' )
 #js = jpm_sp500[['Date' , 'SP500-Close' , 'JPM-Close']]
 combined_df = combined_df.set_index('Date')
@@ -87,18 +86,18 @@ combined_df = combined_df.set_index('Date')
 #js = jpm_sp500[['Date' , 'SP500-Close' , str(stock1).upper() + ' Close']]
 #js = js.set_index('Date')
 print(combined_df)
-print('MY PORTFOLIO vs. SP500')
 print('---------------------------------------------------------')
 
 
-print('CORRELATIONS')
+
 # Calculate Daily Percent Returns
 pct_change = combined_df.pct_change()
+print('Daily % Change')
 print(pct_change)
 
 print('---------------------------------------------------------')
 
-#breakpoint()
+stats = pct_change.describe()
 
 #jpm_percent = jpm.pct_change()  # calculate percent change
 #print(jpm_percent)
@@ -107,9 +106,32 @@ print('---------------------------------------------------------')
 
 #### charting`````
 
-
+print('CORRELATIONS BASED ON DAILY CLOSING PRICES')
 print(combined_df.corr())
+print('---------------------------------------------------------')
 
+print('STATISTICS BASED ON DAILY PERCENT CHANGES')
+stats = pct_change.describe()
+stats = stats.rename(index={ 'std' : 'STD_DEV'  })
+std_dev = stats.loc['STD_DEV']
+#std_dev.rename("STD_DEV.")
+
+fig, ax = plt.subplots()
+std_dev.plot(ax=ax, kind='barh',figsize=(6,4), width=.25, color='lightblue')
+ax.set_title('Highest Volatility', fontsize=14, loc='left')
+ax.set_ylabel('Stock Ticker',color='red',fontsize=12)
+ax.set_xlabel('Daily Standard Deviation',color='red',fontsize=12)
+#ax.set_xlim(0,4)                 
+ax.tick_params(axis='x',labelcolor='blue')
+ax.tick_params(axis='y',labelcolor='blue')
+ax.legend(loc='best')
+#ax.get_children()[list(Best_Player.index).index(y)].set_color('orange')
+plt.show()
+
+
+
+breakpoint()
+print('---------------------------------------------------------')
 fig , ax = plt.subplots()
 pct_change.plot(ax=ax)
 ax.legend(loc = 'best')
