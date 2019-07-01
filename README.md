72fd36a8a6a18e38e6dfaa6c0f8a9de65ef5e5ee
rohanmalhotra4622 <44009676+rohanmalhotra4622@users.noreply.github.com>
2019-06-30 21:52:50 -0400

    Updating README

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
 # Python_Programming_Freestyle_Project
+
+The goal of this project is to extract stock information from Yahoo Finance and run some analytics to help the user make informed decisions to better manage their investments.  In order to extract information we need to import a package called pandas DataReader. Since this is a package it needs to be installed in the environment in which the code will be running.  The instructions to insatll the package are listed below:
+
+pip install pandas-datareader
+
+Depending upon their set up, the user may also need to install other packages. I am leaving that as the responsibilty of the user. 
+
+The user will be required to enter the stocks and the number of shares that they own.  When the user is finished entering the portfolio, they should use 'done' or 'DONE' to see the results.  The code starts of with an empty dictionary labeled 'dic' that will be updated with the stock names as keys and the number of shares as values.  In addition,  there is also an empty list labeled 'portfolio' that will be appended when we enter the stock names.  
+
+The range for which the data is pulled is for the last one year.  The user has the ability ot change this time period. I have set the variable 'end_date'  as todays date and the beginning date with a timedelta of the previous 365 days.  The user can change this beggining date value by inputting a different value in the 'beg_date' variable.
+
+We want to compare our portfolio aginst the S&P 500 index, since that is considered a diversified portfolio.  The code will pull the data for the SP500 index and our portfolio of stocks inputted. Several varibales and dataframes will be created that are mentioned below and numbered.
+
+1) portfolio --> initially an empty list that gets appended based on inputs
+
+2) dic --> initially an empty dictionary that gets updated based on inputs
+
+3) end_date --> todays date
+
+4) beg_date --> end_date - timedelta(365)
+
+5) my_portfolio --> this is a DF of the stocks we entered for the time range choosing only the Adj. Close Column.
+
+6) combined_df --> this is a DF that has the same contents as the DF above with the SP500 values.  This is done by merging the SP500 values retrived with our stock portfolio values.
+
+7) pct_change --> this is DF of percent changes of the combined_df listed in the line above.  This is our measure of volatility.
+
+8) corr --> this is a DF of correlations based on daily closing prices and the not the percent changes.
+
+5) stats --> this is a DF based on daily percent changes and displays key statistical measures.
+
+9) std_dev -->  this is a series, which can be though of as a one column DF that has been extracted from the stats DF.  We sort the values in this series to identify the the most and least volatile stocks so we can display those with more transparecy in the plots.
+
+10) value_stocks -->  this is another empty dictionary that gets updated to reflect the stock names as keys and the values of those stocks based on (shares owned * last stock prices) as values.  
+
+11) ex --> this is a list comprehension that gives the magnitude by which we want the pies to explode in the pie chart. This is needed to make the code dynamic.  
+
+12) explode --> Converting the list 'ex' above into a tuple to be used in code for creating the pie chart.
+
+13) most_volatile_stock --> self explanatory. 
+
+14) least_volatile_stock --> self explanatory.
+
+We then create a bar chart of the volatility of the stocks and color the  most and least volatile stocks differently from the rest.  This is done based on the std_dev series that was extracted from stats.
+
+We then create a pie chart showing the percentages owned based on total value of particular stocks.I also made the pie pieces explode to better visualize the data. 
+
+We also create a line graph based on 'pct_changes' to show volatility over time.  Please note that this last feature will not add a lot of value if our porfolio has many stocks as the visualization will be cluttered. It works well for one or two stocks. The user has the option of entering just one or two stocks to comapre the volatility of their stocks with the SP500 over the time period.
+
+Finally, the DF created are also saved as .csv files in the same location where the code is located. This will help the user if they want to look at the raw data.  
+
+
+
+
+
+
+
+
+
+
+
+
+
--- stocks.py
+++ stocks.py
@@ -1,7 +1,5 @@
 
 
-
-#from pandas.io.data import DataReader
 #pip install pandas-datareader
 import os
 import sys
@@ -9,31 +7,18 @@ import pandas as pd
 import pandas_datareader as pdr
 from datetime import datetime , date, time, timedelta
 import matplotlib.pyplot as plt
+import statsmodels.formula.api as smf
 
-
+## Define URL's to which csv data will be posted
 #url  = r'C:\Users\Rohan Malhotra\Documents\GitHub\Python_Programming_Freestyle_Project\stats.xlsx'
 portfolio_url = os.path.join(os.path.dirname(__file__),  "portfolio.csv")
 combined_portfolio_url = os.path.join(os.path.dirname(__file__),  "combined_portfolio.csv")
 percent_change_url = os.path.join(os.path.dirname(__file__),  "percent_changes.csv")
 correlations_url = os.path.join(os.path.dirname(__file__),  "correlation.csv")
 stats_url = os.path.join(os.path.dirname(__file__),  "stats.csv")
-'''
-portfolio = []
-while True:
-    try:
-        #if isinstance(int(selected_id) , int):
-            #sys.exit()
-        selected_id = input("Please enter stock: " )
-        if type(selected_id) == str:
-            portfolio.append(selected_id)
-        if selected_id == 'DONE' or selected_id == 'done':
-            break
-    except ValueError:
-        print('Entry seems invalid; Please try again:')
-        sys.exit()
 
-portfolio = portfolio[:-1]
-'''
+## Create an empty list and dictionary and append/update based on data inputted. 
+
 portfolio = []
 dic = {}
 while True:
@@ -50,31 +35,26 @@ while True:
         print('Entry seems invalid; Please try again:')
         sys.exit()
 
+
 portfolio = portfolio[:-1]
 
-
+### Define time range
 end_date = datetime.today()
 beg_date = end_date - timedelta(365)
 
-# Define the Index, this should be the benchmark we want to compare against.  
+# Pull data for the SP500; this should be the benchmark we want to compare against.  
+
 print('---------------------------------------------------------')
-#sp500 = pdr.get_data_yahoo(symbols='^gspc', start=datetime(2019, 5, 10), end=datetime(2019, 6, 24))
 sp500 = pdr.get_data_yahoo(symbols='^gspc', start=beg_date , end=end_date)
-#sp500 = sp500[[ 'Open','Adj Close']]
 sp500 = sp500['Adj Close']
 sp500 = pd.DataFrame(sp500).reset_index()
 sp500.columns = ['Date','SP500']
 sp500 = sp500.round(2)
-#sp500 = sp500.rename(columns={ 'Open' : 'SP500-Open'  , 'Adj Close':'SP500-Close'})
-#sp500 = sp500.rename(columns={ 'Adj Close':'SP500-Close'})
-#sp500 = sp500.reset_index()
-#print(sp500)
-
 print('---------------------------------------------------------')
 print('MY PORTFOLIO')
-# Convert portfolio, which is a list into a dataframe
 
-#df = pd.DataFrame({'Ticker':portfolio} )
+### Pull data for our portfolio of stocks
+
 my_portfolio = pdr.get_data_yahoo(portfolio, start=beg_date, end=end_date)
 my_portfolio = my_portfolio['Adj Close'].reset_index()
 my_portfolio = my_portfolio.round(2)
@@ -82,27 +62,11 @@ print(my_portfolio)
 my_portfolio.to_csv(portfolio_url)
 print('---------------------------------------------------------')
 
-#jpm = pdr.get_data_yahoo(symbols=stock1, start=datetime(2019, 4, 10), end=datetime(2019, 6, 24))
-#jpm = pdr.get_data_yahoo(symbols=stock1, start=beg_date , end=end_date)
-#breakpoint()
-##Index(['High', 'Low', 'Open', 'Close', 'Volume', 'Adj Close'], dtype='object')
-#jpm = jpm[[ 'Open','Adj Close']]
-##jpm = jpm.rename(columns={ 'Open' : 'JPM-Open'  , 'Adj Close':'JPM-Close'})
-#jpm = jpm.rename(columns={ 'Open' : str(stock1).upper() + ' Open' , 'Adj Close': str(stock1).upper() + ' Close'})
-#jpm = jpm.reset_index()
-#print(jpm)
-
-
-
 
 # Merge the  dataframes
 print('MY PORTFOLIO vs. SP500')
 combined_df = sp500.merge(my_portfolio, left_on='Date',right_on= 'Date', how='inner' )
-#js = jpm_sp500[['Date' , 'SP500-Close' , 'JPM-Close']]
 combined_df = combined_df.set_index('Date')
-
-#js = jpm_sp500[['Date' , 'SP500-Close' , str(stock1).upper() + ' Close']]
-#js = js.set_index('Date')
 print(combined_df)
 combined_df.to_csv(combined_portfolio_url)
 print('---------------------------------------------------------')
@@ -114,23 +78,18 @@ pct_change = combined_df.pct_change()
 print('Daily % Change')
 pct_change.to_csv(percent_change_url)
 print(pct_change)
-
 print('---------------------------------------------------------')
 
-#jpm_percent = jpm.pct_change()  # calculate percent change
-#print(jpm_percent)
-#sp500_percent = sp500.pct_change()  # calculate percent change
-#print(sp500_percent)
-
-#### charting`````
+## Print Correlations based on daily closing prices
 
 print('CORRELATIONS BASED ON DAILY CLOSING PRICES')
-print(combined_df.corr().round(2))
-combined_df.corr().round(2).to_csv(correlations_url)
+corr = combined_df.corr().round(4)
+print(corr)
+corr.to_csv(correlations_url)
 print('---------------------------------------------------------')
 
 print('STATISTICS BASED ON DAILY PERCENT CHANGES')
-stats = pct_change.describe()
+stats = pct_change.describe().round(5)
 stats = stats.rename(index={ 'std' : 'STD_DEV'  })
 print(stats)
 stats.to_csv(stats_url)
@@ -143,16 +102,18 @@ most_volatile_stock= most_volatile.index[0]
 least_volatile_stock= most_volatile.index[-1]
 
 
+# Create a dictionary of stock symbol as keys and total in dollar amount as values
+value_stocks = {}
+for i in list(dic.keys()):
+    value_stocks.update({i: int(dic[i]) * my_portfolio[i].iloc[-1] })
 
-
-
+## Create bar chart of the volatility of the stocks and color most and least volatile differently from the rest
 
 fig, ax = plt.subplots()
 std_dev.plot(ax=ax, kind='barh',figsize=(6,4), width=.25, color='blue')
 ax.set_title('Highest Volatility', fontsize=20, loc='left')
 ax.set_ylabel('Stock Ticker',color='red',fontsize=16)
-ax.set_xlabel('Daily Standard Deviation',color='red',fontsize=16)
-#ax.set_xlim(0,4)                 
+ax.set_xlabel('Daily Standard Deviation',color='red',fontsize=16)                
 ax.tick_params(axis='x',labelcolor='blue')
 ax.tick_params(axis='y',labelcolor='blue')
 ax.legend(loc='best')
@@ -160,38 +121,20 @@ ax.get_children()[list(std_dev.index).index(most_volatile_stock)].set_color('ora
 ax.get_children()[list(std_dev.index).index(least_volatile_stock)].set_color('red')
 plt.show()
 
-# Create a dictionary of stock symbol as keys and total in dollar amount as values
-value_stocks = {}
-#print(list(dic.keys()))
-#print(len(list(dic.keys())))
-for i in list(dic.keys()):
-    value_stocks.update({i: int(dic[i]) * my_portfolio[i].iloc[-1] })
-
-
-## Create Pie Chart showing value by percent owned
-# my_portfolio['jpm'].iloc[-1]
-ex = [0.1 for i in range(len(list(value_stocks.keys())))]
-#ex = []
-#for q in range(len(list(value_stocks.keys()))):
-#    ex.append(q/10)
+## create pie chart 
+## determine magnitude by which to explode the pie pieces.
+ex = [0.1 for i in range(len(list(value_stocks.keys())))]  
 explode = tuple(ex)
-#print(explode)
 
 labels = list(value_stocks.keys())
 sizes = list(value_stocks.values())
-explode
-#plt.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%', shadow=True, startangle=40)
 plt.title('% Stock Values' , bbox={'facecolor':'0.5', 'pad':4} , fontsize = 14 )
 plt.pie(sizes, explode = explode ,labels = labels, autopct='%1.1f%%', shadow=True, startangle=140)
 plt.axis('equal')
 plt.show()
-#breakpoint()
-
-
 
 
 # Time Series Line Graph for percent changes
-
 
 print('---------------------------------------------------------')
 fig , ax = plt.subplots()
@@ -205,6 +148,10 @@ ax.tick_params(axis='y',labelcolor='blue')
 #ax.ticklabel_format(useOffset = False)
 plt.show()
 
+
+#reg_table = smf.ols("'" + most_volatile_stock + ' ~ ' + 'SP500' + "'" , data=combined_df).fit().summary()
+#reg_table = smf.ols('XES ~ SP500' , data=combined_df).fit().summary()
+#print(reg_table)
 breakpoint()
 
 print('hello')
